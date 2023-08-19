055bf5198383b9b46610ecb623f88b275534afb2
mj2068 <yuelianghushenfu@sina.com>
2023-08-19 09:54:12 +0800

    impl different models(inheritance) for paths

--- sqlmodel_tutorial_fastapi/models.py
+++ sqlmodel_tutorial_fastapi/models.py
@@ -1,10 +1,21 @@
 from sqlmodel import Field, SQLModel
 
 
-class Hero(SQLModel, table=True):
-    __tablename__ = "heroes"  # type: ignore
-
-    id: int | None = Field(default=None, primary_key=True)
+class HeroBase(SQLModel):
     name: str
     secret_name: str
     age: int | None = Field(default=None)
+
+
+class Hero(HeroBase, table=True):
+    __tablename__ = "heroes"  # type: ignore
+
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class HeroCreate(HeroBase):
+    pass
+
+
+class HeroRead(HeroBase):
+    id: int
--- sqlmodel_tutorial_fastapi/routers/heroes.py
+++ sqlmodel_tutorial_fastapi/routers/heroes.py
@@ -1,18 +1,27 @@
 from typing import Annotated
 from fastapi import APIRouter, Body, Depends
-from sqlmodel import Session
+from sqlmodel import Session, select
 from sqlmodel_tutorial_fastapi.db import get_session
 
-from sqlmodel_tutorial_fastapi.models import Hero
+from sqlmodel_tutorial_fastapi.models import Hero, HeroCreate
 
 
 router = APIRouter(prefix="/heroes")
 
 
-@router.post("")
-def create_hero(hero: Annotated[Hero, Body()], session: Session = Depends(get_session)):
-    session.add(hero)
+@router.post("", response_model=Hero)
+def create_hero(
+    hero: Annotated[HeroCreate, Body()], session: Session = Depends(get_session)
+):
+    hero_db = Hero.from_orm(hero)
+    session.add(hero_db)
     session.commit()
-    session.refresh(hero)
+    session.refresh(hero_db)
 
-    return hero
+    return hero_db
+
+
+@router.get("", response_model=list[Hero])
+def read_heroes(session: Session = Depends(get_session)):
+    heroes = session.exec(select(Hero)).all()
+    return heroes
